a697b7d151308f15b9ab2dcc295c839528eb85b6
Denis Feoktistov <nonameden@gmail.com>
2015-07-27 09:17:28 +1200

    Initial commit

---
+++ FootballScores/app/src/main/java/barqsoft/footballscores/Utilies.java
@@ -0,0 +1,85 @@
+package barqsoft.footballscores;
+
+/**
+ * Created by yehya khaled on 3/3/2015.
+ */
+public class Utilies {
+    public static final int SERIE_A = 357;
+    public static final int PREMIER_LEGAUE = 354;
+    public static final int CHAMPIONS_LEAGUE = 362;
+    public static final int PRIMERA_DIVISION = 358;
+    public static final int BUNDESLIGA = 351;
+
+    public static int getLeagueResId(int league_num) {
+        switch (league_num) {
+            case SERIE_A:
+                return R.string.legue_seria_a;
+            case PREMIER_LEGAUE:
+                return R.string.legue_premier;
+            case CHAMPIONS_LEAGUE:
+                return R.string.legue_uefa_champ;
+            case PRIMERA_DIVISION:
+                return R.string.legue_primera_division;
+            case BUNDESLIGA:
+                return R.string.legue_bundesliga;
+            default:
+                return R.string.legue_unknown;
+        }
+    }
+
+    public static int getMatchDayResId(int match_day, int league_num) {
+        if (league_num == CHAMPIONS_LEAGUE) {
+            if (match_day <= 6) {
+                return R.string.match_group_stages;
+            } else if (match_day == 7 || match_day == 8) {
+                return R.string.match_first_knockout;
+            } else if (match_day == 9 || match_day == 10) {
+                return R.string.match_quarter_final;
+            } else if (match_day == 11 || match_day == 12) {
+                return R.string.match_semi_final;
+            } else {
+                return R.string.match_final;
+            }
+        } else {
+            return R.string.match_day;
+        }
+    }
+
+    public static String getScores(int home_goals, int awaygoals) {
+        if (home_goals < 0 || awaygoals < 0) {
+            return " - ";
+        } else {
+            return String.valueOf(home_goals) + " - " + String.valueOf(awaygoals);
+        }
+    }
+
+    public static int getTeamCrestByTeamName(String teamname) {
+        if (teamname == null) {
+            return R.drawable.no_icon;
+        }
+        switch (teamname) {
+            case "Arsenal London FC":
+                return R.drawable.arsenal;
+            case "Manchester United FC":
+                return R.drawable.manchester_united;
+            case "Swansea City":
+                return R.drawable.swansea_city_afc;
+            case "Leicester City":
+                return R.drawable.leicester_city_fc_hd_logo;
+            case "Everton FC":
+                return R.drawable.everton_fc_logo1;
+            case "West Ham United FC":
+                return R.drawable.west_ham;
+            case "Tottenham Hotspur FC":
+                return R.drawable.tottenham_hotspur;
+            case "West Bromwich Albion":
+                return R.drawable.west_bromwich_albion_hd_logo;
+            case "Sunderland AFC":
+                return R.drawable.sunderland;
+            case "Stoke City FC":
+                return R.drawable.stoke_city;
+            default:
+                return R.drawable.no_icon;
+        }
+    }
+}
